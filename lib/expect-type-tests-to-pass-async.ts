7e69b9e584dc6f64d457ef7b383689558442591f
Zach Olivare <zach@olivare.net>
2023-04-19 10:39:48 -0500

    refactor: Seperate out jest specific code
    Theoretically everything except what's in expect-type-tests-to-pass-async
    in this library could apply to any test running, not just jest.

--- lib/expect-type-tests-to-pass-async.ts
+++ lib/expect-type-tests-to-pass-async.ts
@@ -1,17 +1,4 @@
-import fs from 'fs/promises'
-import tsd, {type TsdResult} from 'tsd-lite'
-
-import {convertPathToTypeDefTest} from './path-massager'
-import {formatTsdResults} from './formatter'
-
-type ShortTsdResult = {
-  messageText: TsdResult['messageText']
-  file: {
-    pos: number | undefined
-    end: number | undefined
-    fileName: string | undefined
-  }
-}
+import {getTsdResults} from './get-tsd-results'
 
 /**
  * A helper function to assist with running static TS type definition tests
@@ -26,38 +13,29 @@ type ShortTsdResult = {
  * function.
  */
 export async function expectTypeTestsToPassAsync(pathToTypeDefTest: string) {
-  const testFile = convertPathToTypeDefTest(pathToTypeDefTest)
+  const {
+    allTestsPassed,
+    assertionsCount,
+    testFilePath,
 
-  try {
-    await fs.access(testFile, fs.constants.F_OK)
-  } catch (e: unknown) {
-    const err = e as NodeJS.ErrnoException
-    throw new Error(`The type definition test at "${err.path}" does not exist"`)
-  }
-
-  const {assertionsCount, tsdResults} = tsd(testFile)
-
-  const shortResults: ShortTsdResult[] = tsdResults.map((r) => ({
-    messageText: r.messageText,
-    file: {
-      pos: r.file?.pos,
-      end: r.file?.end,
-      fileName: r.file?.fileName,
-    },
-  }))
+    tsdResults,
+    shortResults,
+    errorCodeFrame,
+  } = await getTsdResults(pathToTypeDefTest)
 
   // Don't pass to jest or log the full results because they make
   // the jest output unreadable. But the full results are still
   // available via the return value though if the user need them
   // for diagnostic purposes.
-  if (shortResults.length) console.error(formatTsdResults(tsdResults))
+  if (!allTestsPassed) console.error(errorCodeFrame)
   expect(shortResults).toHaveLength(0)
 
   return {
+    receivedPath: pathToTypeDefTest,
+    testFilePath,
+
     assertionsCount,
     tsdResults,
     shortResults,
-    receivedPath: pathToTypeDefTest,
-    testFile,
   }
 }
---
+++ lib/get-tsd-results.ts
@@ -0,0 +1,46 @@
+import fs from 'fs/promises'
+import tsd, {type TsdResult} from 'tsd-lite'
+
+import {convertPathToTypeDefTest} from './path-massager'
+import {formatTsdResults} from './formatter'
+
+type ShortTsdResult = {
+  messageText: TsdResult['messageText']
+  file: {
+    pos: number | undefined
+    end: number | undefined
+    fileName: string | undefined
+  }
+}
+
+export async function getTsdResults(pathToTypeDefTest: string) {
+  const testFilePath = convertPathToTypeDefTest(pathToTypeDefTest)
+
+  try {
+    await fs.access(testFilePath, fs.constants.F_OK)
+  } catch (e: unknown) {
+    const err = e as NodeJS.ErrnoException
+    throw new Error(`The type definition test at "${err.path}" does not exist"`)
+  }
+
+  const {assertionsCount, tsdResults} = tsd(testFilePath)
+
+  const shortResults: ShortTsdResult[] = tsdResults.map((r) => ({
+    messageText: r.messageText,
+    file: {
+      pos: r.file?.pos,
+      end: r.file?.end,
+      fileName: r.file?.fileName,
+    },
+  }))
+
+  return {
+    allTestsPassed: shortResults.length === 0,
+    assertionsCount,
+    testFilePath,
+
+    tsdResults,
+    shortResults,
+    errorCodeFrame: formatTsdResults(tsdResults),
+  }
+}
