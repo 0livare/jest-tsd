cd481bea2ac0946f6d4e6207622f025327aa2364
Zach Olivare <zach@olivare.net>
2023-04-21 12:58:10 -0500

    feat(utils): Add findClosingBraceIndex fn

--- lib/utils.ts
+++ lib/utils.ts
@@ -45,9 +45,7 @@ export function parseJsScopeBlocks(str: string) {
 
     if ((str[i] === '"' || str[i] === "'") && !inLineComment && !inBlockComment) {
       inString = !inString
-    }
-
-    if (str[i] === '/' && str[i + 1] === '/' && !inString && !inBlockComment) {
+    } else if (str[i] === '/' && str[i + 1] === '/' && !inString && !inBlockComment) {
       inLineComment = true
       i++
     } else if (str[i] === '\n' && inLineComment) {
@@ -90,4 +88,39 @@ export function parseJsScopeBlocks(str: string) {
     .flat(5)
 
   return [...scopeBlocks, ...nestedBlocks]
+}
+
+/**
+ * Look for the first closing brace in the string starting at `openBraceIndex`,
+ * and return the index of the closing brace that matches with that open brace.
+ */
+export function findClosingBraceIndex(str: string, openBraceIndex: number) {
+  let openBraces = 0
+  let inLineComment = false
+  let inBlockComment = false
+  let inString = false
+
+  for (let i = openBraceIndex; i < str.length; i++) {
+    if ((str[i] === '"' || str[i] === "'") && !inLineComment && !inBlockComment) {
+      inString = !inString
+    } else if (str[i] === '/' && str[i + 1] === '/' && !inString && !inBlockComment) {
+      inLineComment = true
+      i++
+    } else if (str[i] === '\n' && inLineComment) {
+      inLineComment = false
+    } else if (str[i] === '/' && str[i + 1] === '*' && !inString && !inLineComment) {
+      inBlockComment = true
+      i++
+    } else if (str[i] === '*' && str[i + 1] === '/' && inBlockComment) {
+      inBlockComment = false
+      i++
+    } else if (str[i] === '{' && !inLineComment && !inBlockComment && !inString) {
+      openBraces++
+    } else if (str[i] === '}' && !inLineComment && !inBlockComment && !inString) {
+      openBraces--
+      if (openBraces === 0) return i
+    }
+  }
+
+  return -1
 }
