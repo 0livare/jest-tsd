6dd39d8bd9c88047743005a5e9add9324774771b
Zach Olivare <zach@olivare.net>
2023-04-21 21:39:16 -0500

    feat: Read and write files to allow editing the compiled file contents
    I wish this wasn't necessary because obviously file reading and
    writing is kind of slow.
    
    But so far as I can tell, both tsd-lite and @tsd/typescript only
    accept a file path, not a string for the file, so I don't have a
    way to manipulate the file to support test.skip() or test.only()
    without writing to a separate file.
    
    ---
    
    NOTE!!!
    
    The node-tmp and tmp-promise libraries both say repeatedly that
    it is not necessary to manually call the cleanup() function. I'm
    going to trust that in that because awiating the cleanup function
    really slows down the tests.
    
    See:
    https://github.com/raszi/node-tmp#asynchronous-file-creation
    https://github.com/benjamingr/tmp-promise#asynchronous-file-creation

--- lib/get-tsd-results.ts
+++ lib/get-tsd-results.ts
@@ -1,9 +1,11 @@
 import fs from 'fs/promises'
 import tsd, {type TsdResult} from 'tsd-lite'
 import chalk from 'chalk'
+import {file} from 'tmp-promise'
 
 import {convertPathToTypeDefTest} from './path-massager'
 import {formatTsdResults} from './formatter'
+import {commentOutSkippedBlocks} from './test-def-manipulator'
 
 type ShortTsdResult = {
   messageText: TsdResult['messageText']
@@ -16,22 +18,12 @@ type ShortTsdResult = {
 
 export async function getTsdResults(pathToTypeDefTest: string) {
   const testFilePath = convertPathToTypeDefTest(pathToTypeDefTest)
+  await ensureTestFileExists({testFilePath, pathToTypeDefTest})
 
-  try {
-    await fs.access(testFilePath, fs.constants.F_OK)
-  } catch (e: unknown) {
-    const err = e as NodeJS.ErrnoException
-    const jestTestFileName = pathToTypeDefTest.split('/').pop()
-    throw new Error(
-      [
-        `No type definition test file found.`,
-        `File does not exist: ${err.path}`,
-        chalk.bold.yellow(
-          `Did you forget to create your \`.test-d.ts\` file for ${jestTestFileName}?`,
-        ),
-      ].join('\n'),
-    )
-  }
+  const {path} = await file({postfix: '.test-d.ts'})
+  let fileText = await fs.readFile(testFilePath, {encoding: 'utf8'})
+  fileText = commentOutSkippedBlocks(fileText)
+  await fs.writeFile(path, fileText)
 
   const {assertionsCount, tsdResults} = tsd(testFilePath)
 
@@ -52,5 +44,25 @@ export async function getTsdResults(pathToTypeDefTest: string) {
     tsdResults,
     shortResults,
     formattedMaybeErrorResults: formatTsdResults(tsdResults),
+  }
+}
+
+async function ensureTestFileExists(args: {testFilePath: string; pathToTypeDefTest: string}) {
+  const {testFilePath, pathToTypeDefTest} = args
+
+  try {
+    await fs.access(testFilePath, fs.constants.F_OK)
+  } catch (e: unknown) {
+    const err = e as NodeJS.ErrnoException
+    const jestTestFileName = pathToTypeDefTest.split('/').pop()
+    throw new Error(
+      [
+        `No type definition test file found.`,
+        `File does not exist: ${err.path}`,
+        chalk.bold.yellow(
+          `Did you forget to create your \`.test-d.ts\` file for ${jestTestFileName}?`,
+        ),
+      ].join('\n'),
+    )
   }
 }
